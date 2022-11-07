339df2dd15279264603cdda6313e768b7e37e658
gonzaloRocamora <gonza.rocamora@gmail.com>
2022-11-07 09:44:47 +0100

    nav style touch

--- portfolio/src/components/nav/Nav.jsx
+++ portfolio/src/components/nav/Nav.jsx
@@ -4,22 +4,32 @@ import { FcAbout } from "react-icons/fc";
 import { MdPermContactCalendar } from "react-icons/md";
 import { HiDocumentText } from "react-icons/hi";
 import "./nav.css";
+import { useState } from "react";
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
-    
- <nav>
-      <a href="#"><AiOutlineHome /></a>
-      <a href="#about"><FcAbout /></a>
-      <a href="#experience"><HiDocumentText /></a>
-      <a href="#contact"><MdPermContactCalendar /></a>
+    <nav>
+      <a href="#" className={activeNav == "#" ? "active" : ""}>
+        <AiOutlineHome />
+      </a>
+      <a href="#about" 
+            className={activeNav === "about"? 'active' : ''}
+            onClick={() => setActiveNav("#about")}>
+        <FcAbout />
+      </a>
+      <a href="#experience"
+            className={activeNav === "experience"? 'active' : ''}
+            onClick={() => setActiveNav("experience")}>
+        <HiDocumentText />
+      </a>
+      <a href="#contact"
+        className={activeNav === "contact"? 'active' : ''}
+        onClick={() => setActiveNav("contact")}>
+        <MdPermContactCalendar />
+      </a>
     </nav>
-
-   
-   
-
-    
-    
   );
 }
 
